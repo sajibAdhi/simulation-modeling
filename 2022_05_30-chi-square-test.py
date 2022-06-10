6b0f07876b57630e4df985ce0ef81c68ab4f3691
Sajib Adhikary <60180521+sajibAdhi@users.noreply.github.com>
2022-06-10 15:59:51 +0000

    chi-square-test updated

--- 2022_05_30-chi-square-test.py
+++ 2022_05_30-chi-square-test.py
@@ -1,17 +1,38 @@
 # chi square test inputting directly value of Oi
-
-totalClass = int(input("Enter total class: "))
+totalData = 0
 Oi = []
 N = 0
 Dobs = 0
 
-for ptr in range(totalClass):
-    tempOi = float(input(f"Enter Oi for class no {ptr + 1}: "))
-    Oi.append(tempOi)
-    N += tempOi
+userOrFile = input("Enter 'user' to input values manually or 'file' to input values from a file: ")
+
+if userOrFile == 'user':
+    totalData = int(input("Enter total data: "))
+
+    for ptr in range(totalData):
+        tempOi = float(input(f"Enter Oi for class no {ptr + 1}: "))
+        Oi.append(tempOi)
+        N += tempOi
+else:
+    #get file object
+    f = open("data-set.txt", "r")
+
+    while(True):
+        #read next line
+        line = f.readline()
+        #if line is empty, you are done with all lines in the file
+        if not line:
+            break
+        totalData += 1
+        #you can access the line
+        Oi.append(float(line.strip()))
+        N+= float(line.strip())
+
+    #close file
+    f.close
 
 print("All Oi :", Oi)
-Ei = N / totalClass
+Ei = N / totalData
 
 for datum in Oi:
     Dobs += pow((datum - Ei), 2) / Ei
