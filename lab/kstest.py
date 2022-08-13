7db44a6f14eaf59fa247857db12c28083fd8bd8c
Sajib Adhikary <60180521+sajibAdhi@users.noreply.github.com>
2022-08-13 12:53:43 +0000

    kstest update

--- lab/kstest.py
+++ lab/kstest.py
@@ -1,27 +1,35 @@
+# import sqrt
+from math import sqrt
 import random
 
 N = int(input("Enter the size of random numbers to be produced : "))
-D_plus =[]
-D_minus =[]
-_random =[]
+D_plus = []
+D_minus = []
+_random = []
 
 # Rank the N random numbers
 for i in range(0, N):
-	_random.append(random.random())
-	_random.sort()
+    _random.append(random.random())
+    _random.sort()
 
 # Calculate max(i/N-Ri)
 for i in range(1, N + 1):
-	x = i / N - _random[i-1]
-	D_plus.append(x)
+    x = i / N - _random[i-1]
+    D_plus.append(x)
 
 # Calculate max(Ri-((i-1)/N))
 for i in range(1, N + 1):
-	y =(i-1)/N
-	y =_random[i-1]-y
-	D_minus.append(y)
+    y = (i-1)/N
+    y = _random[i-1]-y
+    D_minus.append(y)
 
 # Calculate max(D+, D-)
 ans = max(sqrt(N) * D_plus, sqrt(N) * D_minus)
 print("Value of D is :")
 print(ans)
+
+dalpha = float(input("Enter the value of D alpha : "))
+if ans > dalpha:
+    print("The null hypothesis is rejected")
+else:
+    print("The null hypothesis is not rejected")
